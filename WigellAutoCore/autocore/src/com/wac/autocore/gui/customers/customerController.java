b44f0004b1f8e4ec3ed430d67deeb8fb7109cd2b
unknown <vivieri555@gmail.com>
2026-09-14 10:40:34 +0200

    create Customer form

--- WigellAutoCore/autocore/src/com/wac/autocore/gui/customers/customerController.java
+++ WigellAutoCore/autocore/src/com/wac/autocore/gui/customers/customerController.java
@@ -2,17 +2,21 @@ package com.wac.autocore.gui.customers;
 
 import com.wac.autocore.data.Database;
 import com.wac.autocore.model.Customer;
+import com.wac.autocore.service.GarageSystem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.List;
 
 public class customerController {
+
+    GarageSystem garageSystem;
 
     @FXML
     private TableView<Customer> customerListView;
@@ -28,6 +32,39 @@ public class customerController {
 
     @FXML
     private TableColumn<Customer, String> customerEmailColumn;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField emailField;
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private void handleCreateCustomer() {
+        String name = nameField.getText().trim();
+        String phone = phoneField.getText().trim();
+        String email = emailField.getText().trim();
+
+        try {
+            Customer customer = garageSystem.createCustomer(name, phone, email);
+            statusLabel.setText("Customer "+ customer + " created!");
+            clearFields();
+        }
+        catch (Exception e) {
+            statusLabel.setText("Customer "+ name + " could not be created!");
+        }
+    }
+
+    private void clearFields() {
+        nameField.clear();
+        phoneField.clear();
+        emailField.clear();
+    }
 
     @FXML
     public void initialize() {
