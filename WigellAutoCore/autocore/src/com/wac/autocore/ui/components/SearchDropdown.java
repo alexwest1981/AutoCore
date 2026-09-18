7be8d646abc1c00f9ff4378483152802ec88022c
Alex Weström <inkedalex@gmail.com>
2026-09-18 11:24:24 +0200

    fix(lint): ta bort oanvänd import i SearchDropdown och redundant annotering i SearchResultsView

--- WigellAutoCore/autocore/src/com/wac/autocore/ui/components/SearchDropdown.java
+++ WigellAutoCore/autocore/src/com/wac/autocore/ui/components/SearchDropdown.java
@@ -23,7 +23,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Popup;
 import javafx.stage.Window;
