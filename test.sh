dc879230c4137b57e03409a9a446ddcfa0fd4807
Daniel Eriksson <danieleriksson026@gmail.com>
2026-09-22 18:33:12 +0200

    feat(persistence): lägg till sqlite-jdbc i lib och på classpath (SCRUM-82)

--- test.sh
+++ test.sh
@@ -15,11 +15,26 @@ JAVAC_BIN="$JAVA_HOME/bin/javac"
 SRC_DIR="$DIR/WigellAutoCore/autocore/src"
 RES_DIR="$DIR/WigellAutoCore/autocore/src/resources"
 OUT_DIR="$DIR/out/production/Systemarkitektur"
+JDBC_JAR="$DIR/WigellAutoCore/autocore/lib/sqlite-jdbc-3.53.4.0.jar"
 
 mkdir -p "$OUT_DIR"
 
 # Kompilera alla källfiler inklusive tester
-"$JAVAC_BIN" -d "$OUT_DIR" -sourcepath "$SRC_DIR:$RES_DIR" $(find "$SRC_DIR" -name "*.java")
+mapfile -t SOURCES < <(findProjektarbete feature/persistens-grund  ? ❯ git status
+                           On branch feature/persistens-grund
+                           Your branch is up to date with 'origin/feature/persistens-grund'.
+
+                           Changes not staged for commit:
+                             (use "git add <file>..." to update what will be committed)
+                             (use "git restore <file>..." to discard changes in working directory)
+                                   modified:   test.sh
+
+                           Untracked files:
+                             (use "git add <file>..." to include in what will be committed)
+                                   WigellAutoCore/autocore/lib/
+
+                           no changes added to commit (use "git add" and/or "git commit -a") "$SRC_DIR" -name "*.java")
+"$JAVAC_BIN" -d "$OUT_DIR" -sourcepath "$SRC_DIR:$RES_DIR" -cp "$JDBC_JAR" "${SOURCES[@]}"
 
 # Kör test-runner
-exec "$JAVA_BIN" -cp "$OUT_DIR" com.wac.autocore.test.TestRunner "$@"
+exec "$JAVA_BIN" -cp "$OUT_DIR:$JDBC_JAR" com.wac.autocore.test.TestRunner "$@"
